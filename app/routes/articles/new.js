3a55bc7afdf33bc58be2088a4aeea45279ed6030
Andreas Ntalakas <antalakas@crypteianetworks.com>
2015-02-07 13:54:57 +0100

    deactivate hook in articles/new root

--- app/routes/articles/new.js
+++ app/routes/articles/new.js
@@ -5,6 +5,19 @@ export default Ember.Route.extend({
 		return this.store.createRecord('article', {
 			friend: this.modelFor('friends/show') });
 	}, 
+	deactivate: function() {
+		console.log('----- deactivate hook called -----');
+		
+		// We grab the model loaded in this route //
+		var model = this.modelFor('articles/new');
+		
+		// If we are leaving the Route we verify if the model is in // 'isNew' state, which means it wasn't saved to the backend. //
+		if (model.get('isNew')) {
+			// We call DS#destroyRecord() which removes it from the store //
+			model.destroyRecord();
+		}
+
+	},
 	actions: {
 		save: function() {
 				var _this = this;
